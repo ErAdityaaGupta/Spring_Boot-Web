1bb7270683d75f8bd2bd596431c547b6d39be4e0
Aditya Gupta <ingenious.adig@gmail.com>
2024-07-19 14:19:52 +0530

    Initial Commit

--- SimpleWebapp/src/main/java/com/gupta/SimpleWebapp/Controller/HomeController.java
+++ SimpleWebapp/src/main/java/com/gupta/SimpleWebapp/Controller/HomeController.java
@@ -1,6 +1,5 @@
-package com.gupta.SimpleWebapp;
+package com.gupta.SimpleWebapp.Controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ SimpleWebapp/src/main/java/com/gupta/SimpleWebapp/Controller/loginController.java
@@ -0,0 +1,9 @@
+package com.gupta.SimpleWebapp.Controller;
+
+public class loginController {
+
+    public String login(){
+        return "Login Page...";
+    }
+
+}
