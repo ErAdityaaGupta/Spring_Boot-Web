355ebf49a7546303ed6c372ba36aa81da04a2872
Aditya Gupta <ingenious.adig@gmail.com>
2024-07-25 13:56:49 +0530

    Initial Commit

--- SimpleWebapp/src/main/java/com/gupta/SimpleWebapp/Model/Product.java
+++ SimpleWebapp/src/main/java/com/gupta/SimpleWebapp/Model/Product.java
@@ -1,5 +1,7 @@
 package com.gupta.SimpleWebapp.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public  class Product {
 
+    @Id
     private int prodId;
     private String prodName;
     private int price;
