7096cb92b29eb0b405b07de38f0ee47f11fdf39d
Aditya Gupta <ingenious.adig@gmail.com>
2024-07-25 13:57:05 +0530

    Initial Commit

--- SimpleWebapp/src/main/java/com/gupta/SimpleWebapp/Service/ProductService.java
+++ SimpleWebapp/src/main/java/com/gupta/SimpleWebapp/Service/ProductService.java
@@ -1,6 +1,8 @@
 package com.gupta.SimpleWebapp.Service;
 
 import com.gupta.SimpleWebapp.Model.Product;
+import com.gupta.SimpleWebapp.Repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,42 +12,52 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products = new ArrayList<>(Arrays.asList(new Product(001,"Phone",60000),
-                                           new Product(002,"Ipad",90000)));
+    @Autowired
+    ProductRepo repo;
+
+//    List<Product> products = new ArrayList<>(Arrays.asList(new Product(001,"Phone",60000),
+//                                           new Product(002,"Ipad",90000)));
 
     public List<Product> getProducts(){
-        return products;
+//        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int prodId) {
-        return products.stream()
-                .filter(p -> p.getProdId() == prodId)
-                .findFirst().orElse(new Product(000,"No Item Found",0));
+//        return products.stream()
+//                .filter(p -> p.getProdId() == prodId)
+//                .findFirst().orElse(new Product(000,"No Item Found",0));
+        return repo.findById(prodId).orElse(new Product());
     }
 
     public void addProduct(Product prod){
-        products.add(prod);
+//        products.add(prod);
+        repo.save(prod);
     }
 
     public void updateProduct(Product prod) {
-        int index = 0;
-        for(int i =0; i<products.size();i++){
-            if(products.get(i).getProdId() == prod.getProdId()){
-                index = i;
-            }
-        }
+//        int index = 0;
+//        for(int i =0; i<products.size();i++){
+//            if(products.get(i).getProdId() == prod.getProdId()){
+//                index = i;
+//            }
+//        }
+//
+//        products.set(index,prod);
 
-        products.set(index,prod);
+        repo.save(prod);
     }
 
     public void deleteProduct(int prodId) {
-        int index = 0;
-        for(int i =0; i<products.size();i++){
-            if(products.get(i).getProdId() == prodId){
-                index = i;
-            }
-        }
+//        int index = 0;
+//        for(int i =0; i<products.size();i++){
+//            if(products.get(i).getProdId() == prodId){
+//                index = i;
+//            }
+//        }
+//
+//        products.remove(index);
 
-        products.remove(index);
+        repo.deleteById(prodId);
     }
 }
